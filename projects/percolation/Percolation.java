932d9ab1c66b2d9f93acfe0b5b8109e801f7584d
huangkai <h1770360848@outlook.com>
2020-08-30 21:57:49 +0800

    fix(part I week1): percolation array index out of bound

--- projects/percolation/Percolation.java
+++ projects/percolation/Percolation.java
@@ -3,35 +3,55 @@ package percolation;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * @author huangkai
+ */
 public class Percolation {
-    // The uf with virtual top site and virtual bottom site
-    private final WeightedQuickUnionUF uf;
+    /**
+     * The uf with virtual top site and virtual bottom site
+     */
+    private final WeightedQuickUnionUF uf1;
 
-    // The uf with virtual top site
-    private final WeightedQuickUnionUF backwashUf;
+    /**
+     * The uf with virtual top site
+     */
+    private final WeightedQuickUnionUF uf2;
 
-    // The width of the grid
+    /**
+     * The width of the grid
+     */
     private final int width;
 
-    // The sites of grid, true represents `opened`, false represents `blocked`
+    /**
+     * The sites of grid, true represents `opened`, false represents `blocked`
+     */
     private final boolean[][] sites;
 
-    // The number of open sites
+    /**
+     * The number of open sites
+     */
     private int openedCount = 0;
 
-    // The index of virtual top site
+    /**
+     * The index of virtual top site
+     */
     private final int topIndex;
 
-    // The index of virtual bottom site
+    /**
+     * The index of virtual bottom site
+     */
     private final int bottomIndex;
 
-    // creates n-by-n grid, with all sites initially blocked
+    /**
+     * creates n-by-n grid, with all sites initially blocked
+     */
     public Percolation(int n) {
-        if (n <= 0)
+        if (n <= 0) {
             throw new IllegalArgumentException("`n` must be positive");
+        }
 
-        uf = new WeightedQuickUnionUF(n * n + 2);
-        backwashUf = new WeightedQuickUnionUF(n * n + 1);
+        uf1 = new WeightedQuickUnionUF(n * n + 2);
+        uf2 = new WeightedQuickUnionUF(n * n + 1);
         width = n;
         sites = new boolean[n][n];
         for (int i = 0; i < n; i++) {
@@ -43,48 +63,63 @@ public class Percolation {
         bottomIndex = n * n + 1;
     }
 
-    // opens the site (row, col) if it is not open already
+    /**
+     * opens the site (row, col) if it is not open already
+     */
     public void open(int row, int col) {
-        int index = getIndex(row, col);
-        sites[row - 1][col - 1] = true;
-        openedCount++;
+        if (!isOpen(row, col)) {
+            int index = getIndex(row, col);
+            sites[row - 1][col - 1] = true;
+            openedCount++;
 
-        if (row == 1) {
-            uf.union(index, topIndex);
-            uf.union(index, topIndex);
+            if (row == 1) {
+                uf1.union(index, topIndex);
+                uf2.union(index, topIndex);
+            }
+
+            if (row == width) {
+                uf1.union(index, bottomIndex);
+            }
+
+            // union the current site with it's left, right, top, bottom site
+            // if neighbour not exists, skip this
+            union(row, col, row - 1, col);
+            union(row, col, row + 1, col);
+            union(row, col, row, col - 1);
+            union(row, col, row, col + 1);
         }
-
-        if (row == width) {
-            uf.union(index, bottomIndex);
-        }
-
-        // union the current site with it's left, right, top, bottom site
-        // if neighbour not exists, skip this
-        union(row, col, row - 1, col);
-        union(row, col, row + 1, col);
-        union(row, col, row, col - 1);
-        union(row, col, row, col + 1);
     }
 
-    // is the site (row, col) open?
+    /**
+     * @return is the site (row, col) open?
+     */
     public boolean isOpen(int row, int col) {
+        if (row <= 0 || row > width || col <= 0 || col > width) {
+            throw new IllegalArgumentException("The given site (row, col) must in the n-by-n grid");
+        }
         return sites[row - 1][col - 1];
     }
 
-    // is the site (row, col) full?
+    /**
+     * @return is the site (row, col) full?
+     */
     public boolean isFull(int row, int col) {
         int index = getIndex(row, col);
-        return backwashUf.find(index) == backwashUf.find(topIndex);
+        return uf2.find(index) == uf2.find(topIndex);
     }
 
-    // returns the number of open sites
+    /**
+     * @return the number of open sites
+     */
     public int numberOfOpenSites() {
         return openedCount;
     }
 
-    // does the system percolate?
+    /**
+     * @return does the system percolate?
+     */
     public boolean percolates() {
-        return uf.find(topIndex) == uf.find(bottomIndex);
+        return uf1.find(topIndex) == uf1.find(bottomIndex);
     }
 
     public static void main(String[] args) {
@@ -106,21 +141,30 @@ public class Percolation {
      */
     private int getIndex(int row, int col) {
         if (row <= 0 || row > width || col <= 0 || col > width) {
-            throw new IndexOutOfBoundsException("The given site (row, col) must in the n-by-n grid");
+            throw new IllegalArgumentException("The given site (row, col) must in the n-by-n grid");
         }
 
         return (row - 1) * width + col;
     }
 
-    private void union(int p_row, int p_col, int q_row, int q_col) {
-        if (q_row <= 0 || q_row > width || q_col <= 0 || q_col > width)
+    /**
+     * union two sites
+     *
+     * @param pRow the row of first site
+     * @param pCol the col of first site
+     * @param qRow the row of first site
+     * @param qCol the col of first site
+     */
+    private void union(int pRow, int pCol, int qRow, int qCol) {
+        if (qRow <= 0 || qRow > width || qCol <= 0 || qCol > width) {
             return;
+        }
 
-        int pIndex = getIndex(p_row, p_col);
-        int qIndex = getIndex(q_row, q_col);
-        if (isOpen(q_row, q_col)) {
-            uf.union(pIndex, qIndex);
-            backwashUf.union(pIndex, qIndex);
+        int pIndex = getIndex(pRow, pCol);
+        int qIndex = getIndex(qRow, qCol);
+        if (isOpen(qRow, qCol)) {
+            uf1.union(pIndex, qIndex);
+            uf2.union(pIndex, qIndex);
         }
     }
 }
